454aaca35c3604bc37a54f95720b27bb18eac7e0
unknown <mfaqihuddin23@gmail.com>
2023-07-18 15:38:11 +0700

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,41 @@
 module.exports = {
   content: ["./assets/**/*.{html,js}", "./pages/**/*.{html,js}", "./index.html", ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'veni': {
+          100: '',
+          200: '',
+          300: '',
+          400: '',
+          500: '',
+          600: '',
+          700: '',
+          800: '',
+          900: '',
+        },
+      },
+      fontFamily: {
+        clashV: "ClashDisplay-Variable",
+        clashE: "ClashDisplay-Extralight",
+        clashL: "ClashDisplay-Light",
+        clashR: "ClashDisplay-Regular",
+        clashM: "ClashDisplay-Medium",
+        clashS: "ClashDisplay-Semibold",
+        clashB: "ClashDisplay-Bold",
+      },
+    },
+    container: {
+      center: true,
+      screens:{
+        xl:'1224px'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
 
