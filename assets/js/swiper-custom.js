8e5cc874d27d9b34a1bde93368873126a4a6230c
unknown <mfaqihuddin23@gmail.com>
2023-07-30 22:38:29 +0700

    projectdetails done

--- assets/js/swiper-custom.js
+++ assets/js/swiper-custom.js
@@ -44,3 +44,40 @@ const swiperblog = new Swiper('.swiper-blog', {
     el: '.swiper-scrollbar',
   },
 });
+const swiperdetails = new Swiper('.swiper-details', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      
+    },
+    // when window width is >= 480px
+    480: {
+      
+    },
+    // when window width is >= 640px
+    640: {
+      spaceBetween: 32
+    }
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
